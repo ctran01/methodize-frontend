a7db43d4e683be2834b8a1b4ea921eab2eb30a7e
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2021-01-03 01:12:59 -0600

    Added project links to home page projects

--- src/components/projects/ProjectItemHome.js
+++ src/components/projects/ProjectItemHome.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../../css/Project.css";
 import { AiOutlineEllipsis } from "react-icons/ai";
 import { Menu, MenuItem } from "@material-ui/core";
@@ -33,36 +34,41 @@ const ProjectItemHome = ({ project }) => {
   // );
 
   return (
-    <div className="project-home-item" onClick={openModal}>
-      <div className="project-home-item-inner-container">
-        <div className="project-home-item-inner-left">
-          <div className="project-home-item-icon-container">
-            <span className={`dot-task-${project.id}`}></span>
+    <Link
+      className="project-tile--link"
+      to={`/team/${project.team_id}/project/${project.id}/${project.name}`}
+    >
+      <div className="project-home-item" onClick={openModal}>
+        <div className="project-home-item-inner-container">
+          <div className="project-home-item-inner-left">
+            <div className="project-home-item-icon-container">
+              <span className={`dot-task-${project.id}`}></span>
+            </div>
+            <div className="project-home-item-name-container">
+              <p style={{ fontSize: "15px", fontWeight: "500", margin: "0px" }}>
+                {project.name}
+              </p>
+            </div>
           </div>
-          <div className="project-home-item-name-container">
-            <p style={{ fontSize: "15px", fontWeight: "500", margin: "0px" }}>
-              {project.name}
-            </p>
+          <div
+            className="project-home-item-more-menu"
+            style={{ height: "100%" }}
+            onClick={handleMenuClick}
+          >
+            <AiOutlineEllipsis style={{ fontSize: "24px" }} />
           </div>
+          <Menu
+            style={{}}
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Delete</MenuItem>
+          </Menu>
         </div>
-        <div
-          className="project-home-item-more-menu"
-          style={{ height: "100%" }}
-          onClick={handleMenuClick}
-        >
-          <AiOutlineEllipsis style={{ fontSize: "24px" }} />
-        </div>
-        <Menu
-          style={{}}
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem onClick={handleMenuClose}>Delete</MenuItem>
-        </Menu>
       </div>
-    </div>
+    </Link>
   );
 };
 
