95228007b7271d9aa1ddfd0ca5c214bb9d0be4b5
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2021-01-26 16:59:07 -0600

    Added error logging

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import "./index.css";
 import App from "./App";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
+import LogRocket from "logrocket";
+// Sentry.init({
+//   dsn:
+//     "https://4acb30e87b26473babb209dd1ae07e67@o505304.ingest.sentry.io/5593396",
+//   autoSessionTracking: true,
+//   integrations: [new Integrations.BrowserTracing()],
 
-Sentry.init({
-  dsn:
-    "https://4acb30e87b26473babb209dd1ae07e67@o505304.ingest.sentry.io/5593396",
-  autoSessionTracking: true,
-  integrations: [new Integrations.BrowserTracing()],
+//   // We recommend adjusting this value in production, or using tracesSampler
+//   // for finer control
+//   tracesSampleRate: 1.0,
+// });
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
+LogRocket.init("mcmykl/methodize");
 
 ReactDOM.render(
   <React.StrictMode>
